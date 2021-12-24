ec45a4e53b76a2a3414138042c612f5de5794a4b
Márcio Daurí <mdauri@gmail.com>
2021-12-24 12:17:14 -0300

    challenge completed

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,5 +1,5 @@
-import { User } from "../../model/User";
-import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository, ICreateUserDTO } from '../IUsersRepository';
 
 class UsersRepository implements IUsersRepository {
   private users: User[];
@@ -19,23 +19,43 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+    Object.assign(user, {
+      name,
+      email,
+      admin: false,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find(user => user.id === id);
+
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find(user => user.email === email);
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const reponseUser = receivedUser;
+
+    reponseUser.admin = true;
+
+    return reponseUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    const all = this.users;
+
+    return all;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,23 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+import { ListAllUsersUseCase } from './ListAllUsersUseCase';
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    let { user_id } = request.body;
+    try {
+      if (!user_id) {
+        user_id = request.header('user_id');
+      }
+
+      const all = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(all);
+    } catch (error) {
+      return response.status(400).send({ error: 'User is not admin' });
+    }
   }
 }
 
